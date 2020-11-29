ef507483eab69ab055ac5832a5e86bab3fbf2296
enjoymylif <noru0916@gmail.com>
2020-11-29 14:20:25 +0700

    Products from commerce.js

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
+import { Products, NavBar } from "./components";
+
+const Index = () => {
+  const [products, setProducts] = useState([]);
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      <Products products={products} />
+    </div>
+  );
+};
+
+export default Index;
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -14,17 +14,25 @@ const Product = ({ product }) => {
   const classes = useStyles();
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image={""} title={product.name} />
+      <CardMedia
+        className={classes.media}
+        image={product.media.source}
+        title={product.name}
+      />
       <CardContent>
         <div className={classes.cardContent}>
-          <Typography variant={"h5"} gutterBottom>
+          <Typography variant={"h2"} gutterBottom>
             {product.name}
           </Typography>
-          <Typography variant={"h5"}>{product.price}</Typography>
+          <Typography variant={"h2"}>
+            {product.price.formatted_with_symbol}
+          </Typography>
         </div>
-        <Typography variant={"h2"} color={"textSecondary"}>
-          {product.description}
-        </Typography>
+        <Typography
+          variant={"h5"}
+          color={"textSecondary"}
+          dangerouslySetInnerHTML={{ __html: product.description }}
+        />
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
         <IconButton aria-label={"Add to Card"}>
