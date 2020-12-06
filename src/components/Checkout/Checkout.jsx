8acd93159e3e12e7e0c326ddeec4a1414a7173d6
enjoymylif <noru0916@gmail.com>
2020-12-06 14:30:05 +0700

    Review Component

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -42,7 +42,7 @@ const Checkout = ({ cart }) => {
     activeStep === 0 ? (
       <AddressForm checkoutToken={checkoutToken} next={next} />
     ) : (
-      <PaymentForm />
+      <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} />
     );
 
   const Confirmation = () => <div>Confirmation</div>;
--- src/components/Checkout/PaymentForm/PaymentForm.jsx
+++ src/components/Checkout/PaymentForm/PaymentForm.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Typography, Button, Divider } from "@material-ui/core";
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js/pure";
+import Review from "./Review/Review";
 
-const PaymentForm = () => {
-  return <div>PaymentFOrm</div>;
+const PaymentForm = ({ checkoutToken }) => {
+  return (
+    <>
+      <Review checkoutToken={checkoutToken} />
+    </>
+  );
 };
 
 export default PaymentForm;
